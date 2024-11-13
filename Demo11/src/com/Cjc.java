e19ed0c4d89ba680d07acb631652840055d0d25a
poojapatil <poojapatil1622@gmail.com>
2024-11-13 13:10:19 +0530

    by gauri

--- Demo11/src/com/Cjc.java
+++ Demo11/src/com/Cjc.java
@@ -2,12 +2,18 @@ package com;
 
 public class Cjc 
 {
-public static void main(String[] args) {
-	System.out.println("welcome to cjc");
-}
 	public void m1()
 	{
 			System.out.println("M1 method");
 
 	}
+	public void m2()
+	{
+			System.out.println("M2 method");
+
+	}
+public static void main(String[] args) {
+	System.out.println("welcome to cjc");
+}
+	
 }
